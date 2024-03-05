44dd281c4d581771e32bc0499db8e9bc9c355dd1
gmahima <gmahima@Gs-MacBook-Air.local>
2024-03-05 16:54:06 +0530

    add theme example

--- src/stories/Avatar.stories.tsx
+++ src/stories/Avatar.stories.tsx
@@ -5,8 +5,16 @@ import {Avatar} from "../components/Avatar";
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
   title: "Example/Avatar",
-  component: Avatar,
-  argTypes: {},
+  component: ({theme, ...args}) => {
+    return <div className={theme}><Avatar {...args} /></div>
+  },
+  argTypes: {
+    theme: {
+      control: 'radio', // Use radio buttons for selection
+      options: ['light', 'dark'], // Define the options for the theme
+      description: 'The theme for the Avatar component', // A brief description of what this arg does
+    },
+  },
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
     layout: "centered",
@@ -20,5 +28,9 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
-export const Primary: Story = {};
+export const Primary: Story = {
+  args: {
+    theme: "dark"
+  }
+};
 
